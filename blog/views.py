1cec89b29785a24eb86c3ed93c078b6167bff9fe
leanvega1 <leandrogvega@gmail.com>
2019-06-06 16:05:25 -0300

    add posts in pages (two posts for page)

--- blog/views.py
+++ blog/views.py
@@ -5,12 +5,17 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+
 
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    paginator = Paginator(posts, 2)  # Show 25 contacts per page
+    page = request.GET.get('page')
+    mini_posts = paginator.get_page(page)
+    return render(request, 'blog/post_list.html', {'posts': mini_posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
